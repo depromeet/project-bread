d7aca043f53786d091fdf33104c4f78d68779ac7
yhk <yhk1903@naver.com>
2016-08-13 03:14:45 +0900

    commit layout_xml

---
+++ app/src/main/java/com/depromeet/bread/activity/AgreementActivity.java
@@ -0,0 +1,26 @@
+package com.depromeet.bread.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.depromeet.bread.R;
+
+/**
+ * Created by 권윤환 on 2016-08-11.
+ */
+public class AgreementActivity extends AppCompatActivity{
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.agreement);
+
+
+
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/depromeet/bread/activity/UnivSelectActivity.java
+++ app/src/main/java/com/depromeet/bread/activity/UnivSelectActivity.java
@@ -1,26 +1,28 @@
 package com.depromeet.bread.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.depromeet.bread.R;
 
-public class UnivSelectActivity extends Activity {
+public class UnivSelectActivity extends AppCompatActivity {
 
     ImageButton univBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.univ_select);
+
+
 
         //대학 선택 버튼
         univBtn = (ImageButton) findViewById(R.id.univBtn);
         univBtn.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick( View v ){
+
 
             }
         });
