c366fd73d714cbfd26439c7cc51d5b2b40a0024d
yhk <yhk1903@naver.com>
2016-08-15 17:03:39 +0900

    commit xml_layout

--- app/src/main/java/com/depromeet/bread/activity/UnivSelectActivity.java
+++ app/src/main/java/com/depromeet/bread/activity/UnivSelectActivity.java
@@ -1,5 +1,6 @@
 package com.depromeet.bread.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -22,8 +23,8 @@ public class UnivSelectActivity extends AppCompatActivity {
         univBtn.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick( View v ){
-
-
+                Intent intent = new Intent(getApplicationContext(), UnivListActivity.class);
+                startActivity(intent);
             }
         });
     }
